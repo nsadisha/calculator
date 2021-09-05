fdf3be7f758794aeac95f199bafcca550bbe7076
nsadisha <nsadisha@gmail.com>
2021-09-05 20:09:08 +0530

    code for the version 3

--- src/java/com.kelaniya.uni/v3/CommandLineInputs.java
+++ src/java/com.kelaniya.uni/v3/CommandLineInputs.java
@@ -9,14 +9,14 @@ public class CommandLineInputs {
         //Making sure that there are arguments
         if (args.length == 0){
             System.out.println("Provide the operator as an argument");
-//            return;
+            return "";
         }
 
         String operator = args[0];
 
         if ( !(operator.equals("add") || operator.equals("sub") || operator.equals("mul") || operator.equals("div")) ){
             System.out.println("Please provide add, sub, mul, or div as the operator");
-//            return;
+            return "";
         }
         return operator;
     }
