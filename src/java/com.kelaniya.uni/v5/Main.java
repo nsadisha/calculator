e44f53586ae41f33373bdf73732f2b20314f1466
nsadisha <nsadisha@gmail.com>
2021-09-12 17:22:57 +0530

    calling the cordinator class and get the work done

--- src/java/com.kelaniya.uni/v5/Main.java
+++ src/java/com.kelaniya.uni/v5/Main.java
@@ -1,22 +1,22 @@
 package com.kelaniya.uni.v5;
 
-import com.kelaniya.uni.v5.operation.Operation;
+import com.kelaniya.uni.v5.input.CommandLineInputs;
+import com.kelaniya.uni.v5.input.Inputs;
 import com.kelaniya.uni.v5.operation.OperationFactory;
 import com.kelaniya.uni.v5.repository.FileNumberRepository;
+import com.kelaniya.uni.v5.repository.NumberRepository;
+import com.kelaniya.uni.v5.ui.CmdLineUI;
+import com.kelaniya.uni.v5.ui.UI;
 
 public class Main {
     public static void main(String[] args) throws Exception{
 
-        CommandLineInputs inputs = new CommandLineInputs(args);
-        String operator = inputs.getOperator();
-
-        FileNumberRepository fileReader = new FileNumberRepository();
-        double[] nums = fileReader.getNumbers();
-
+        Inputs inputs = new CommandLineInputs(args);
+        NumberRepository numberRepository = new FileNumberRepository();
         OperationFactory operationFactory = new OperationFactory();
-        Operation operation = operationFactory.getInstance(operator);
-        double result = operation.execute(nums);
+        UI ui = new CmdLineUI();
+        CalculatorApp app = new CalculatorApp(inputs, numberRepository, operationFactory, ui);
+        app.execute();
 
-        UI.showMessage("Result is: "+result);
     }
 }
