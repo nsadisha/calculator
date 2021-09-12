a7d4ca563abe3d0a70da461fd1346e23044aad29
nsadisha <nsadisha@gmail.com>
2021-09-12 16:34:37 +0530

    numbers file moved out from the project

--- src/java/com.kelaniya.uni/v5/repository/FileNumberRepository.java
+++ src/java/com.kelaniya.uni/v5/repository/FileNumberRepository.java
@@ -5,11 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class FileNumberRepository {
+public class FileNumberRepository implements NumberRepository{
     public double[] getNumbers() throws IOException {
         //Read numbers form the text file
         List<String> numbers = Files.readAllLines(
-                Paths.get("src/java/com.kelaniya.uni/v2/in/numbers.txt")
+//                Paths.get("src/java/com.kelaniya.uni/v2/in/numbers.txt")
+                Paths.get("D:/numbers.txt")
         );
 
         double num1 = Double.parseDouble(numbers.get(0));
