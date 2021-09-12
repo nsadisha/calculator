f71bd5a30b75a1006fbe2edeb99e2092efb5ccca
nsadisha <nsadisha@gmail.com>
2021-09-12 17:31:59 +0530

    SQL number repository

---
+++ src/java/com.kelaniya.uni/v5/repository/SqlNumberRepository.java
@@ -0,0 +1,10 @@
+package com.kelaniya.uni.v5.repository;
+
+import java.io.IOException;
+
+public class SqlNumberRepository implements NumberRepository{
+    @Override
+    public double[] getNumbers() throws IOException {
+        return new double[0];
+    }
+}
