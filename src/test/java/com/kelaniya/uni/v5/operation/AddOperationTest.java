576fca72c47941f38b81ead8c33b628534105c26
nsadisha <nsadisha@gmail.com>
2021-09-19 21:10:29 +0530

    all the test cases AddOperation

--- src/test/java/com/kelaniya/uni/v5/operation/AddOperationTest.java
+++ src/test/java/com/kelaniya/uni/v5/operation/AddOperationTest.java
@@ -23,5 +23,26 @@ public class AddOperationTest {
 
         assertThat(result, is(11.0));
     }
+    @Test
+    public void should_add_zeros(){
+        AddOperation addOperation = new AddOperation();
+        double result = addOperation.execute(new double[]{0, 0});
 
+        assertThat(result, is(0.0));
+    }
+    @Test
+    public void should_add_negative_values(){
+        AddOperation addOperation = new AddOperation();
+        double result = addOperation.execute(new double[]{-5, -3});
+
+        assertThat(result, is(-8.0));
+    }
+    @Test
+    public void should_add_positive_and_negative_values(){
+        AddOperation addOperation = new AddOperation();
+        double result = addOperation.execute(new double[]{5, -3});
+
+        assertThat(result, is(2.0));
+    }
+    
 }
