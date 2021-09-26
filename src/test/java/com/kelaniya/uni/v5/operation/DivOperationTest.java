4ae1fda400b7362c5c3cc9cd934a256edb9b833e
nsadisha <nsadisha@gmail.com>
2021-09-26 21:35:13 +0530

    unit tests for DivOperation

--- src/test/java/com/kelaniya/uni/v5/operation/DivOperationTest.java
+++ src/test/java/com/kelaniya/uni/v5/operation/DivOperationTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertThrows;
 
 public class DivOperationTest {
 
@@ -16,11 +15,23 @@ public class DivOperationTest {
         assertThat(result, is(2.0));
     }
     @Test
+    public void should_divide_negative_values() throws InvalidCalcOperationException {
+        DivOperation divOperation = new DivOperation();
+        double result = divOperation.execute(new double[] {-9.0, -3.0});
+
+        assertThat(result, is(3.0));
+    }
+    @Test
+    public void should_divide_negative_and_positive_values() throws InvalidCalcOperationException {
+        DivOperation divOperation = new DivOperation();
+        double result = divOperation.execute(new double[] {-9.0, 3.0});
+
+        assertThat(result, is(-3.0));
+    }
+    @Test(expected = InvalidCalcOperationException.class)
     public void should_not_divide_by_zero() throws InvalidCalcOperationException {
         DivOperation divOperation = new DivOperation();
         double result = divOperation.execute(new double[] {6.0, 0.0});
-
-        //will be continued
     }
 
 }
